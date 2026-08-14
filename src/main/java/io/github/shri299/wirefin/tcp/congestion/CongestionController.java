5a87c141067fc1242aa2262f657465a5a92c117c
Srishti Srivastava <ssrishti508@gmail.com>
2026-08-14 18:43:28 +0530

    feat: implement NewReno fast recovery state

--- src/main/java/io/github/shri299/wirefin/tcp/congestion/CongestionController.java
+++ src/main/java/io/github/shri299/wirefin/tcp/congestion/CongestionController.java
@@ -4,6 +4,12 @@ public interface CongestionController {
     void onAcknowledgement(int newlyAcknowledgedBytes);
     void onTimeout(long bytesInFlight);
     void onFastRetransmit(long bytesInFlight);
+    default void onFastRetransmit(long bytesInFlight, long recoveryPoint) { onFastRetransmit(bytesInFlight); }
+    default void onDuplicateAck() {}
+    default void onPartialAcknowledgement(int newlyAcknowledgedBytes) {}
+    default void onRecoveryComplete() {}
+    default boolean inFastRecovery() { return false; }
+    default long recoveryPoint() { return 0; }
     long congestionWindow();
     long slowStartThreshold();
 }
